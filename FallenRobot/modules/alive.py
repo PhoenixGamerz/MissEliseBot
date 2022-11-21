54cda14715d49483bc6fbf421226043a89606353
Phoenix Gamerz <114356392+PhoenixGamerz@users.noreply.github.com>
2022-11-21 09:54:35 +0530

    Update alive.py

--- FallenRobot/modules/alive.py
+++ FallenRobot/modules/alive.py
@@ -12,7 +12,7 @@ from FallenRobot import telethn as tbot, SUPPORT_CHAT, OWNER_USERNAME, dispatche
 
 PHOTO = [
     "https://telegra.ph/file/6a21ea6677342f43b363e.jpg",
-    "https://te.legra.ph/file/4bd77f0b39cbf640ce685.jpg",
+    "https://te.legra.ph/file/0fd3876da20a3ee23ccf4.jpg",
 ]
 
 
